0ffc960046a9a2224417f8f8e9ad6e22f723ea86
AQUAiTE <francisiwnlschool@gmail.com>
2024-04-27 20:33:11 -0700

    My styles is back

--- user-interface/App.js
+++ user-interface/App.js
@@ -12,4 +12,13 @@ const App = () => {
   )
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 export default App;
