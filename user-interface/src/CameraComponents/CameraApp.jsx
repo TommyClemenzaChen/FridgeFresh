47e6483ff7714ae716045fc3c594731b81b6e705
Ryan <92900778+ryanokimoto@users.noreply.github.com>
2024-04-27 20:00:00 -0700

    camera components

---
+++ user-interface/src/CameraComponents/CameraApp.jsx
@@ -0,0 +1,86 @@
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function CameraApp() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = ExpoCamera.useCameraPermissions();
+  const [cameraRef, setCameraRef] = useState(null);
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+  const takePicture = async () => {
+    if (cameraRef) {
+      let photo = await cameraRef.takePictureAsync();
+      console.log(photo); // You can handle the photo object as needed
+    }
+  };
+
+
+  return (
+    <View style={styles.container}>
+      <ExpoCamera style={styles.camera} type={type} ref={(ref) => setCameraRef(ref)}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={takePicture}
+          >
+            <Text style={{ fontSize: 20, marginBottom: 10, color: "white" }}>
+              Take Picture
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </ExpoCamera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
