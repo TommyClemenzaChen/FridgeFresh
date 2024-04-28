fca233eb5f678d582b21064a0f07bf9fdf126f80
AQUAiTE <francisiwnlschool@gmail.com>
2024-04-27 20:11:47 -0700

    Non-responsive login page

--- user-interface/App.js
+++ user-interface/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Login />
       <StatusBar style="auto" />
     </View>
   );
