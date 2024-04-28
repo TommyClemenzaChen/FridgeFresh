249d0fe0b5ec9daad08db6bc61646a170113cb07
AQUAiTE <francisiwnlschool@gmail.com>
2024-04-28 02:34:42 -0700

    Moving styles to separate file

--- user-interface/src/pages/SignUp/SignUp.jsx
+++ user-interface/src/pages/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { auth } from "../../services/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import styles from './SignUpStyles';
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -70,33 +71,4 @@ const SignUp = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  root: {
-    alignItems: "center",
-    padding: 20,
-  },
-  logo: {
-    width: "70%",
-    maxWidth: 300,
-
-    height: 100,
-  },
-  text: {
-    color: "gray",
-    marginVertical: 10,
-  },
-  link: {
-    //darker yellow color
-    color: "#FFD700",
-  },
-  input: {
-    width: "100%",
-    height: 40,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-  },
-});
 export default SignUp;
---
+++ user-interface/src/pages/SignUp/SignUpStyles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    root: {
+      alignItems: "center",
+      padding: 20,
+    },
+    logo: {
+      width: "70%",
+      maxWidth: 300,
+  
+      height: 100,
+    },
+    text: {
+      color: "gray",
+      marginVertical: 10,
+    },
+    link: {
+      //darker yellow color
+      color: "#FFD700",
+    },
+    input: {
+      width: "100%",
+      height: 40,
+      borderWidth: 1,
+      borderColor: "#ccc",
+      borderRadius: 5,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+});
+
+export default styles;
